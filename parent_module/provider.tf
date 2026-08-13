43d3da3c6f7571053f73e68608c381c079667bf0
Manas Kar <mkdellpc@gmail.com>
2026-08-13 22:12:06 +0530

    Format Terraform files

--- parent_module/provider.tf
+++ parent_module/provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
-    azurerm ={
-        source ="hashicorp/azurerm"
-        version = "4.80.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.80.0"
     }
   }
-   backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "manas-storage"
     storage_account_name = "manas420"
     container_name       = "tfstate"
@@ -13,5 +13,5 @@ terraform {
   }
 }
 provider "azurerm" {
-    features {}
+  features {}
 }
