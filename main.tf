b8464a73c8c32d3c1f5831f022a9d3b1f3db8404
Manas Kar <mkdellpc@gmail.com>
2026-08-13 21:54:50 +0530

    module add

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "mk_rg"
-  location = var.location
-}
-resource "azurerm_resource_group" "new_rg" {
-  name     = "new-mk-rg"
-  location = "Central India"
-}
